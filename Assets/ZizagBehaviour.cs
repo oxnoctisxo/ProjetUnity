ebc63044884ca37ae83392a088a14c60f5c3d3a5
oxnoctisxo <tip.hon@hotmail.fr>
2016-12-01 21:01:24 +0100

    FFXV GOTY

---
+++ Assets/ZizagBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZizagBehaviour : MonoBehaviour {
+
+    public int speed;
+    public Vector3 direction;
+    public float distance ;
+
+    float distanceInitial;
+    int sens;
+
+
+    void Awake()
+    {
+        distanceInitial = distance;
+        sens = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance -= Time.deltaTime;
+
+        if (distance <= 0)
+        {
+            sens *= -1;
+            distance = distanceInitial;
+            transform.Rotate(direction , 180f);
+        }
+        transform.Translate(direction.normalized * sens * speed * Time.deltaTime, Space.World);
+    }
+
+    
+}
